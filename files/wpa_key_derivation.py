40decdb24945c4893b3d9359ae7ec61677ad467d
Baptiste Hardrick <batach31@hotmail.com>
2020-04-25 18:08:18 +0200

    ended part 1

--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -18,7 +19,8 @@ __status__ 		= "Prototype"
 
 from scapy.all import *
 from binascii import a2b_hex, b2a_hex
-from pbkdf2_math import pbkdf2_hex
+# Ce module n'existe pas
+# from pbkdf2_math import pbkdf2_hex
 from pbkdf2 import *
 from numpy import array_split
 from numpy import array
@@ -43,21 +45,21 @@ wpa=rdpcap("wpa_handshake.cap")
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
-ssid        = "SWI"
-APmac       = a2b_hex("cebcc8fdcab7")
-Clientmac   = a2b_hex("0013efd015bd")
+ssid        = wpa[0].info.decode("utf-8") # le beacon contient le ssid
+APmac       = a2b_hex(wpa[0].addr3.replace(':','')) # il contient aussi l'adresse de l'AP
+Clientmac   = a2b_hex(wpa[1].addr1.replace(':','')) # le client répond avec son adresse
 
 # Authenticator and Supplicant Nonces
-ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+ANonce      = wpa[5].load[13:45] # le premier handshake contient le ANonce # a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
+SNonce      = wpa[6].load[13:45] # le second handshake contient le SNonce # a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
-mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
+mic_to_test = wpa[8].load[77:93] #c'est le 4eme handshake qui contient le mic #"36eef66540fa801ceee2fea9b7929b40" 
 
 B           = min(APmac,Clientmac)+max(APmac,Clientmac)+min(ANonce,SNonce)+max(ANonce,SNonce) #used in pseudo-random function
 
-data        = a2b_hex("0103005f02030a0000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") #cf "Quelques détails importants" dans la donnée
+data        = bytes(wpa[8][EAPOL])[0:77] + b'\x00'*(len(mic_to_test)+6) #we copy until where the mic start, then pad with the length of the mix + some bytes to have only zeros at the end  # a2b_hex("0103005f02030a0000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") #cf "Quelques détails importants" dans la donnée
 
 print ("\n\nValues used to derivate keys")
 print ("============================")
