e6713be3596cff89aad964878daae8ee1e6d463a
Christoph Rueff <christoph.rueff@gmail.com>
2020-04-27 12:43:12 +0200

    Implement mic test loop with algo test, Add wordlist.txt file

--- files/scaircrack.py
+++ files/scaircrack.py
@@ -44,6 +44,7 @@ A           = "Pairwise key expansion" #this string is used in the pseudo-random
 ssid        = wpa[0].info.decode("utf-8") # le beacon contient le ssid
 APmac       = a2b_hex(wpa[0].addr3.replace(':','')) # il contient aussi l'adresse de l'AP
 Clientmac   = a2b_hex(wpa[1].addr1.replace(':','')) # le client répond avec son adresse
+hash_algo   = bytes([wpa[8].load[2:3][0] & b'\x03'[0]]) # extracting the bits that tell us the use algo (sha1 or md5)
 
 # Authenticator and Supplicant Nonces
 ANonce      = wpa[5].load[13:45] # le premier handshake contient le ANonce # a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
@@ -61,16 +62,32 @@ ssid = str.encode(ssid)
 
 # read wordlist line by line
 with open('wordlist.txt') as fp:
-	passPhrase = fp.readline()
-	while passPhrase:
-		pp = str.encode(passPhrase)
-		#calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
-		pmk = pbkdf2(hashlib.sha1,pp, ssid, 4096, 32)
-		#expand pmk to obtain PTK
-		ptk = customPRF512(pmk,str.encode(A),B)
+    passPhrase = fp.readline()[:-1]
+    found = False
+    while passPhrase:
+        pp = str.encode(passPhrase)
+        # calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
+        pmk = pbkdf2(hashlib.sha1,pp, ssid, 4096, 32)
+        # expand pmk to obtain PTK
+        ptk = customPRF512(pmk,str.encode(A),B)
 
-		#calculate MIC over EAPOL payload (Michael)- The ptk is, in fact, KCK|KEK|TK|MICK
-		mic = hmac.new(ptk[0:16],data,hashlib.sha1)
-		
-		if mic.hexdigest()[:len(mic.hexdigest())-8] == b2a_hex(mic).decode()
-		passPhrase = fp.readline()
+        # calculate MIC over EAPOL payload (Michael)- The ptk is, in fact, KCK|KEK|TK|MICK
+
+        # select the hashing algo to use
+        if hash_algo == b'\x02':
+            algo = hashlib.sha1
+        else:
+            algo = hashlib.md5
+
+        # compute mic from pass phrase
+        mic = hmac.new(ptk[0:16],data,algo).digest()[:16]
+
+        # test the computed mic with the mic from the handshake
+        if mic.hex() == mic_to_test.hex():
+            print("The pass phrase is : " + passPhrase)
+            found = True
+            break
+
+        passPhrase = fp.readline()[:-1]
+    if not found:
+        print("Pass phrase not in the list.")
